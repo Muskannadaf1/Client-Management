c4f74484dea512832be2c60e2e2f6440954864a0
Muskannadaf1 <nadaf.muskan2002@gmail.com>
2024-01-22 11:10:09 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,79 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl,ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+import { CreateClientComponent } from './create-client/create-client.component';
+import { ScheduleMeetingComponent } from './schedule-meeting/schedule-meeting.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink,CreateClientComponent,ScheduleMeetingComponent,ReactiveFormsModule,HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client-management-app';
+
+  constructor(private router : Router){}
+  integerRegex = /^\d+$/
+  emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+  title = 'Client Management Application';
+
+  ngOnInit(): void{
+
 }
+
+  registerform = new FormGroup({
+    Name : new FormControl("", [Validators.required,Validators.maxLength(30),Validators.minLength(2)]),
+    Email : new FormControl("",[Validators.required,Validators.maxLength(30),Validators.pattern(this.emailRegex)]),
+    address : new FormControl("", [Validators.required,Validators.maxLength(30)]),
+    password : new FormControl("", [Validators.required,Validators.maxLength(30),Validators.minLength(8)]),
+    confirm_password : new FormControl("", [Validators.required,Validators.maxLength(30),Validators.minLength(8)]),
+
+
+  });
+
+
+
+  registerclient(){
+    console.warn(this.registerform.value)
+  }
+
+
+  get Name():FormControl {
+    return this.registerform.get("Name")as FormControl;
+  }
+
+  onClickFunction(){
+    this.router.navigate(['/createclient'])
+  }
+
+
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CreateClientComponent } from './create-client/create-client.component';
+import { ScheduleMeetingComponent } from './schedule-meeting/schedule-meeting.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'addnewclient', component:CreateClientComponent},
+    {path:'schedulemeet', component:ScheduleMeetingComponent}
+];
